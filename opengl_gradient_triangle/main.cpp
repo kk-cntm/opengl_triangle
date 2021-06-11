a7b0178b7bf541d9b81312ce8eb0a80551e0977e
Kirill Kogut <kirilrempov@gmail.com>
2021-06-11 13:17:11 +0300

    Initial Commit

---
+++ opengl_gradient_triangle/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  opengl_gradient_triangle
+//
+//  Created by Kiril Kogut on 11.06.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
